76ac84199537fe0e48d85d67719da7b55beab60a
lukagbn <luka68690@gmail.com>
2026-09-18 14:50:38 +0400

    fix: handle aces dynamically in blackjack

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,124 @@
+import "./App.sass";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [winner, setWinner] = useState<string>(" ");
-  const [userCards, setUserCards] = useState<number[]>([]);
   const [dealerCards, setDealerCards] = useState<number[]>([]);
-  const [timesUp, setTimesUp] = useState<boolean>(true);
+  const [playerCards, setPlayerCards] = useState<number[]>([]);
+  const [winner, setWinner] = useState<string>("");
+  const [overlay, setOverlay] = useState<boolean>(true);
 
-  function randomCardNumber() {
-    let number = Math.floor(Math.random() * 13) + 1;
-    if (number === 1) return (number = 11);
-    else if (number > 10) return (number = 10);
-    else return number;
+  function randomCard(): number {
+    const cardNumber = Math.floor(Math.random() * 13) + 1;
+    if (cardNumber === 1) return 11;
+    else if (cardNumber > 10) return 10;
+    else return cardNumber;
   }
 
-  useEffect(() => {
-    const userSum = userCards.reduce((acc, curr) => acc + curr, 0);
+  function winningHand(): any {
+    const playerSum = playerCards.reduce((acc, curr) => acc + curr, 0);
     const dealerSum = dealerCards.reduce((acc, curr) => acc + curr, 0);
 
-    if (userSum > 21) {
+    if (dealerSum > playerSum && dealerSum < 21) {
       setWinner("You Lost!");
-    } else if (dealerSum > 21) {
+    } else if (dealerSum > 21 && playerSum < dealerSum) {
       setWinner("You Won!");
-    } else if (userSum === 21) {
-      setWinner("You Won!");
+    } else if (dealerSum === playerSum && playerSum <= 21) {
+      setWinner("Draw!");
     } else if (dealerSum === 21) {
       setWinner("You Lost!");
-    } else if (dealerSum > 18) {
-      if (userSum > dealerSum) {
-        setWinner("You Won!");
-      } else if (userSum === dealerSum) {
-        setWinner("Draw!");
-      } else {
-        setWinner("You Lost!");
-      }
     } else {
-      setWinner(" ");
+      setWinner("You Won!");
     }
-  }, [userCards, dealerCards]);
-
-  function startNewGame() {
-    setUserCards([]);
-    setDealerCards([]);
-    setWinner(" ");
-    setTimesUp(true);
   }
+
+  function startGame(): any {
+    setOverlay(false);
+    setWinner("");
+    setDealerCards([randomCard(), randomCard(), randomCard()]);
+    setPlayerCards([randomCard(), randomCard(), randomCard()]);
+  }
+
+  function restartGame(): any {
+    setOverlay(true);
+    setWinner("");
+    setDealerCards([]);
+    setPlayerCards([]);
+  }
+
+  function winnerColor(winner: string): string {
+    let color = "";
+    switch (winner) {
+      case "You Won!":
+        color = "#9acb36";
+        break;
+      case "You Lost!":
+        color = "red";
+        break;
+      case "Draw!":
+        color = "grey";
+        break;
+    }
+    return color;
+  }
+
+  function dealerDraw() {}
 
   useEffect(() => {
     const dealerSum = dealerCards.reduce((acc, curr) => acc + curr, 0);
-    const usersSum = userCards.reduce((acc, curr) => acc + curr, 0);
+    const playerSum = playerCards.reduce((acc, curr) => acc + curr, 0);
 
-    if (usersSum === 21) return;
-    if (dealerSum > 18 || userCards.length === 0) return;
+    if (dealerCards.includes(11) && dealerSum > 21) {
+      const indexOfAce = dealerCards.findIndex((num) => num === 11);
 
-    const timeout = setTimeout(() => {
-      if (dealerSum > usersSum) return;
-      setDealerCards((prev) => [...prev, randomCardNumber()]);
-      setTimesUp(false);
-    }, 3000);
+      setDealerCards((cards) =>
+        cards.map((card, index) => (index === indexOfAce ? 1 : card)),
+      );
+    }
 
-    return () => clearTimeout(timeout);
-  }, [dealerCards, userCards]);
+    if (playerCards.includes(11) && playerSum > 21) {
+      const indexOfAce = playerCards.findIndex((num) => num === 11);
+
+      setPlayerCards((cards) =>
+        cards.map((card, index) => (index === indexOfAce ? 1 : card)),
+      );
+    }
+  }, [dealerCards, playerCards]);
+
+  useEffect(() => {
+    if (playerCards.length === 0 || dealerCards.length === 0) return;
+
+    const timeOut = setTimeout(
+      () => {
+        dealerDraw();
+        winningHand();
+      },
+      true ? 3000 : 0,
+    );
+    return () => clearTimeout(timeOut);
+  }, [playerCards, dealerCards, winner]);
 
   return (
-    <div>
-      <img src="/dealer.png" alt="dealer" />
-      <div>Dealer Cards: {dealerCards.map((card) => card + " ")}</div>
-      <p>{winner}</p>
-      <div>Your Cards: {userCards.map((card) => card + " ")}</div>
-      <button
-        onClick={() => {
-          if (timesUp) {
-            let sum = userCards.reduce((acc, curr) => acc + curr, 0);
-            if (sum > 21 || sum === 21) return;
-            setUserCards((prev) => [...prev, randomCardNumber()]);
-          } else return;
-        }}
-      >
-        Draw new card
-      </button>
-      <button
-        onClick={() => {
-          startNewGame();
-        }}
-      >
-        Start new game
-      </button>
-    </div>
+    <main className="main">
+      {overlay && (
+        <div className="overlay">
+          <button onClick={() => startGame()}>start</button>
+        </div>
+      )}
+
+      <section className="board">
+        <img src="/dealer.png" alt="dealer" />
+        <div>Dealer Cards: {dealerCards.map((card) => card + " ")}</div>
+        <p className="winner" style={{ color: winnerColor(winner) }}>
+          {!overlay && winner}
+        </p>
+        <div>Your Cards: {playerCards.map((card) => card + " ")}</div>
+        <div className="btnWrapper">
+          <button>hit</button>
+          <button>stand</button>
+          <button onClick={() => restartGame()}>new game</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
